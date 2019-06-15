021558a4832f439238e42cf4277c2575185a6aa1
sdyin <sdyin@foxmail.com>
2019-06-15 11:00:40 +0800

    git add

---
+++ src/main/java/com/sdyin/guava/SpringbootGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.sdyin.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGuavaApplication.class, args);
+	}
+
+}
