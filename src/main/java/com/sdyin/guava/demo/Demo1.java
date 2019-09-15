ec8de6dac58a153d8ebaa7181ae1e87665b01464
sdyin <sdyin@foxmail.com>
2019-09-15 16:11:39 +0800

    git add

--- src/main/java/com/sdyin/guava/demo/Demo1.java
+++ src/main/java/com/sdyin/guava/demo/Demo1.java
@@ -1,9 +1,16 @@
 package com.sdyin.guava.demo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @Description:
  * @Author: liuye
  * @time: 2019/9/15$ 下午3:57$
  */
 public class Demo1 {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext("com.sdyin.guaua");
+        ac.close();
+    }
 }
