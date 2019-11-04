37e6a63b12fdd6899dda254a205dbc1df5a8735d
sdyin <liuye@xforceplus.com>
2019-11-04 14:04:15 +0800

    增加 prometheus 依赖

--- src/main/java/com/sdyin/guava/SpringbootGuavaApplication.java
+++ src/main/java/com/sdyin/guava/SpringbootGuavaApplication.java
@@ -1,7 +1,11 @@
 package com.sdyin.guava;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author liuye
@@ -11,6 +15,11 @@ public class SpringbootGuavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootGuavaApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 
 }
